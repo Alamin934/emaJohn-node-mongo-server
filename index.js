ed0db9ead43293405bc5463198d321cf8a85a136
Alamin934 <islamalamin934@gmail.com>
2022-06-12 13:05:37 +0600

    connect node express with mongodb and send data to client side for diplay

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogrrwih.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('online_shop');
+        const productsCollection = database.collection('products');
+
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Crud Server running succesfully');
