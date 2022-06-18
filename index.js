1f189812e0d01a2f52659dc2e6556148aaa28b66
Alamin934 <islamalamin934@gmail.com>
2022-06-18 16:13:58 +0600

    get jwt token id from client side and get it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const { initializeApp } = require('firebase-admin/app');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,16 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogrrwih.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function verifyToken(req, res, next) {
+    if (req.headers.authorization.startsWith('Bearar ')) {
+        const token = req.headers.authorization.split('Bearar')[1];
+    }
+
+    next();
+}
+
 async function run() {
     try {
         await client.connect();
@@ -48,7 +59,6 @@ async function run() {
 
         //Order Get Method
         app.get('/orders', async (req, res) => {
-            console.log(req.headers.authorization)
             let query = {};
             const email = req.query.email;
             if (email) {
